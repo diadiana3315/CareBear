744abf526b50e265d34995784378b78d320227a3
George <thecosminel@gmail.com>
2024-11-10 12:19:28 +0200

    Updated git ignore and added google auth dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation(libs.play.services.auth)
 //    implementation("com.google.firebase:firebase-database:20.3.0")
 //    implementation("com.google.gms:google-services:4.4.0")
 //    implementation(libs.okhttp-logging-interceptor)
